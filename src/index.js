b2af02ee46af9cb71321833e22c7bbf8c8327370
kxwzhang <kxwzhang@gmail.com>
2020-12-16 18:32:40 -0800

    Write a MyTextInput React component to handle both input and errors at once

--- src/index.js
+++ src/index.js
@@ -91,6 +91,19 @@ import "./styles.css";
 // };
 
 /* Use the Formik components instead */
+ const MyTextInput = ({ label, ...props }) => {
+   const [field, meta] = useField(props);
+   return (
+     <>
+       <label htmlFor={props.id || props.name}>{label}</label>
+       <input className="text-input" {...field} {...props} />
+       {meta.touched && meta.error ? (
+         <div className="error">{meta.error}</div>
+       ) : null}
+     </>
+   );
+ };
+
 const SignupForm = () => {
    return (
     <Formik
